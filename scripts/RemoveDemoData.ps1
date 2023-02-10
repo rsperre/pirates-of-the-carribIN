29ecb5d893525ef2ba2d237522899680972f3403
rsperre <rune.sperre@innovasjonnorge.no>
2023-02-10 10:37:42 +0100

    add cleanup script

---
+++ scripts/RemoveDemoData.ps1
@@ -0,0 +1,41 @@
+# get connection using local connection script
+
+C:\Dev\scripts\connectacdc.ps1
+
+$team = Get-PnPTeamsTeam | Where-Object DisplayName -eq "POS Team Demo"
+
+if ($null -ne $team) {
+    $grp = Get-PnPMicrosoft365Group -Identity $team.GroupId
+    Remove-PnPMicrosoft365Group -Identity $grp.Id
+    
+    # wait for team to be deleted...
+
+    Write-Host -ForegroundColor Gray "Waiting for team to be removed"
+    while ($null -ne $team) {
+        Start-Sleep -Seconds 20
+        Write-Host "." -NoNewline
+        $team = Get-PnPTeamsTeam | Where-Object DisplayName -eq "POS Team Demo"
+    }
+}
+
+#check if we can reuse an existing M365 group
+
+$group = Get-PnPMicrosoft365Group -Identity "POS Team Demo"
+if ($null -ne $group -and $group.hasTeam -eq $false) {
+    Write-Host "Adding Team to existing group" -ForegroundColor DarkGray
+    $newteam = New-PnPTeamsTeam -GroupId $group.GroupId
+}
+else {
+    Write-Host "(re)creating Demo Team" -ForegroundColor DarkGray
+    $newteam = New-PnPTeamsTeam -DisplayName "POS Team Demo"
+}
+
+Write-Host "Waiting for team to be provisioned"
+while ($null -eq $ewteam) {
+    Start-Sleep -Seconds 20
+    Write-Host "." -NoNewline
+    $newteam = Get-PnPTeamsTeam | Where-Object DisplayName -eq "POS Team Demo"
+}
+Write-Host "Adding user(s)"  -ForegroundColor DarkGray
+Add-PnPTeamsUser -Team $newteam -User "runspe@caribin.onmicrosoft.com"
+
