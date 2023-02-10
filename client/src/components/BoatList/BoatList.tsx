f14b4eb5b7cea3868fcc8f31b108d00e301e4e0c
Håvard Hovde <havard.langdal.hovde@innovasjonnorge.no>
2023-02-10 12:57:18 +0100

    hide api keys

--- client/src/components/BoatList/BoatList.tsx
+++ client/src/components/BoatList/BoatList.tsx
@@ -5,6 +5,12 @@ import BoatInfo from "../BoatInfo/BoatInfo";
 const BoatList = () => {
   const [boats, setBoats] = useState<Array<String | Number>>([]);
 
+  // const apiKey = import.meta.env.VITE_AIS_ACCESS_TOKEN;
+
+  // React.useEffect(() => {
+  //   console.log(apiKey);
+  // }, [apiKey]);
+
   const handleClick = async () => {
     const res = await fetch(
       "https://historic.ais.barentswatch.no/v1/historic/mmsiinarea",
@@ -13,8 +19,7 @@ const BoatList = () => {
         headers: {
           Accept: "application/json",
           "Content-type": "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjBCM0I1NEUyRkQ5OUZCQkY5NzVERDMxNDBDREQ4OEI1QzA5RkFDRjNSUzI1NiIsIng1dCI6IkN6dFU0djJaLTctWFhkTVVETjJJdGNDZnJQTSIsInR5cCI6ImF0K2p3dCJ9.eyJpc3MiOiJodHRwczovL2lkLmJhcmVudHN3YXRjaC5ubyIsIm5iZiI6MTY3NjAyNTQwMSwiaWF0IjoxNjc2MDI1NDAxLCJleHAiOjE2NzYwMjkwMDEsImF1ZCI6ImFpcyIsInNjb3BlIjpbImFpcyJdLCJjbGllbnRfaWQiOiJoYXZhcmQubGFuZ2RhbC5ob3ZkZUBpbm5vdmFzam9ubm9yZ2Uubm86YWNkYyJ9.kmZQ2VH8OxNhp1HJ3b7FYomhMhqTU2ffwOrAGW2p_ieCaiLcKNWbdkSb1-g8sYbGr2SUBPkTvfNN6MNMZHbSVJBolSovcCoaSmLXrKM8_3X7LcyiGd_AUgJUhj5M9Wdi2ydHFlAo3tB6wnduiEKYcmkdamkhlL1aqvlQn13k9yaSuxPmC5wLz9as0yLuDvtJGcY3V6qeIw0F_1LdbVoalZPqfIXocCIbaiZsC3sIKp6wuivdnf_F1ZI22KFHz0kQI_90DTMZv4Ca5YZt3zdd0xsLIz29AtWZDH2ze5IjqjTj52lwGyEHiHffoOPVkHJN5IkJX0UxfM3d_y8PUUR4lu55xZ4ckHzTmC1RVJNy7bY1RSKB1jX2DvjN-qrNCeE-Fds3z9jb6zZkUC2Bq9KL0IyMZydHJIrvK7oUz2DLg95fpsEmYsxp9pqBteA5yEsbNe0ucuNTSqez1JgaqPci2KLfvM7aD5d6J0ZfPgBHyj6HWqI8rsVlMIk-CkMUDTQl1Oi_GN6Wrg6BSiJ-BR0-a8cI2zPvRAaImvmu_WElULL0WM50g7mDAWjYclltwwF6KK_CsPfU-aQL5DnG6Y_1_J_B3ML1XPus-bpqDf9ClUX6gWO-S46iG8zeF7w70iGF-IkSvIC_zpqKNb4jUEpEmAer_DHn7do1jrRaAZxGpvw",
+          Authorization: `Bearer ${import.meta.env.VITE_AIS_ACCESS_TOKEN}`,
         },
         body: JSON.stringify({
           msgtimefrom: "2023-02-09T00:00:00+00:00",
